218d5158cf9589101c2db610e24b853a4c23e393
navya290397 <navyaankam29031997@gmail.com>
2025-05-07 12:01:25 +0530

    Create new file from Dify JSON

---
+++ src/main/java/com/hms/repository/RoomRepository.java
@@ -0,0 +1,8 @@
+// RoomRepository.java
+package com.hms.repository;
+
+import com.hms.entity.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
+}
