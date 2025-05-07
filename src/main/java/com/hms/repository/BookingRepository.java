46faaecdc2ba2a6c62fc1a4b0f0a26d5be5453a1
navya290397 <navyaankam29031997@gmail.com>
2025-05-07 12:01:24 +0530

    Create new file from Dify JSON

---
+++ src/main/java/com/hms/repository/BookingRepository.java
@@ -0,0 +1,8 @@
+// BookingRepository.java
+package com.hms.repository;
+
+import com.hms.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+}
